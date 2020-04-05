e85bd65b5d59a214586661d3e9c594c438491180
Dejan Pekter <dejanpe@nordeus.com>
2020-04-05 23:23:14 +0200

    Tomorrow's tasks :)

---
+++ src/com/xeniascrafts/ZadaciTODO.java
@@ -0,0 +1,14 @@
+package com.xeniascrafts;
+
+public class ZadaciTODO {
+
+
+    /**
+     *  https://petlja.org/biblioteka/r/problemi/Zbirka/zbir_cifara
+     *  https://petlja.org/biblioteka/r/problemi/Zbirka/ponoc
+     *  https://petlja.org/biblioteka/r/problemi/Zbirka/tenis
+     *  https://petlja.org/biblioteka/r/problemi/Zbirka/unix_vreme
+     *  https://petlja.org/biblioteka/r/problemi/Zbirka/brojevi_trocifreni_parni
+     */
+    
+}
